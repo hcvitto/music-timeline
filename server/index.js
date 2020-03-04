944d535f94fb31fba623acbf383090014ba92d52
vittorio <hcvitto@gmail.com>
2020-03-04 18:02:44 +0100

    added mongo config and quick crud routes

--- server/index.js
+++ server/index.js
@@ -1,21 +1,57 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var socket = require("socket.io");
+const express = require('express');
+const app = express();
 
-var server = http.createServer(app);
-var io = socket.listen(server);
+const http = require('http');
+
+const body_parser = require("body-parser");
+app.use(body_parser.json());
+
+const port = 3000;
+
+// db setup
+const db = require('./db');
+const dbName = 'music_timeline';
+const collectionName = 'step';
+
+// db init
+db.initialize(dbName, collectionName, function(dbCollection) { // success callback
+  // get all items
+  // dbCollection.find().toArray(function(err, result) {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   console.log(result);
+  // });
+
+  // crud routes
+
+  app.post('/step', (request, response) => {
+    const item = request.body;
+    dbCollection.insertOne(item, (error, result) => {
+      if (error) throw error;
+      dbCollection.find().toArray(function (_error, _result) {
+        if (_error) throw _error;
+        response.json(_result);
+      });
+    })
+  });
+
+  app.get('/steps', (request, response) => {
+    dbCollection.find().toArray(function (_error, _result) {
+      if (_error) throw _error;
+      response.json(_result);
+    });
+  });
+
+}, function (err) {  // failure callback
+    throw err;
+});
+const server = http.createServer(app);
 
 app.get('/', function(req, res) {
   res.send("<h1>Hello world</h1>");
 });
 
-io.on('connection', function(socket) {
-  socket.on("from client", function(data) {
-    io.emit("from server", data);
-  });  
-});
-
-server.listen(3000, function() {
+server.listen(port, function() {
   console.log('listening on *:3000');
 });
